325aab8481be8ca19a4617308fbde57d91d35e6e
ishikagupta19 <ishika2001ishika@gmail.com>
2022-09-29 23:22:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import About from "./About";
 import './App.css';
 import Cart from "./Cart";
@@ -11,7 +12,30 @@ import SingleProduct from "./SingleProduct";
 
 
 function App() {
+
+  const theme = {
+    colors: {
+      heading:"rgb(24,24,29)",
+      text:"rgba(29,29,28,0.8)",
+      white:"#fff",
+      black:"#212529",
+      helper:"#8490ff",
+      bg:"#F6F8FA",
+      footer_bg:"#0a1435",
+      btn:"rgb(98,84,243)",
+      hr:"#ffffff",
+      gradient:"linear-gradient(0deg, rgb(132,144,255) 0%, rgb(98, 189,252) 100%)",
+      shadow:"rgba(0,0,0,0.02) 0px 1px 3px 0px, rgba(27,31,35,0.15) 0px 0px 0px 1px;",
+      shadowSupport: "rgba(0,0,0,0.16) 0px 1px 4px",
+    },
+    media:{
+      mobile: "768px",
+      lab: "998px",
+    },
+  };
+
   return (
+    <ThemeProvider theme={theme}>
     <BrowserRouter>
     <GlobalStyle/>
       <Routes>
@@ -24,6 +48,7 @@ function App() {
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
